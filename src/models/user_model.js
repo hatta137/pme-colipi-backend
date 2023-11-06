eca6d07f415e08121fc6e8a86e59bc7a8c5b2145
hendrik <hendrik.lendeckel@fh-erfurt.de>
2023-11-06 17:43:19 +0100

    edit user model

--- src/models/user_model.js
+++ src/models/user_model.js
@@ -3,8 +3,11 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
     username: String,
+    email: String,
+    password: String,
+    beercounter: Number,
     wg: { type: mongoose.SchemaTypes.ObjectId, ref: "WG" },
-    // ...
+
 });
 
 export default mongoose.model("User", userSchema);
